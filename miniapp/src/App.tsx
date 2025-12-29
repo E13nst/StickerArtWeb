1c5951e0a6891175205b0766473f544194d11676
wind.slw <wind.slw@gmail.com>
2025-12-29 13:36:42 +0300

    Revert "Add sticker generation page replacing NFTSoonPage in navigation"
    This reverts commit 0828649765eaa62a435471590ebb0834d66cfd35.

--- miniapp/src/App.tsx
+++ miniapp/src/App.tsx
@@ -18,7 +18,7 @@ const ProfilePage = lazy(() => import('@/pages/ProfilePage').then(m => ({ defaul
 const MyProfilePage = lazy(() => import('@/pages/MyProfilePage').then(m => ({ default: m.MyProfilePage })));
 const DashboardPage = lazy(() => import('@/pages/DashboardPage').then(m => ({ default: m.DashboardPage })));
 const AuthorPage = lazy(() => import('@/pages/AuthorPage').then(m => ({ default: m.AuthorPage })));
-const StickerGeneratePage = lazy(() => import('@/pages/StickerGeneratePage').then(m => ({ default: m.StickerGeneratePage })));
+const NftSoonPage = lazy(() => import('@/pages/NftSoonPage').then(m => ({ default: m.NftSoonPage })));
 
 // TON Connect manifest URL (статический, так как MiniApp развёрнут на стабильном домене)
 const manifestUrl = 'https://sticker-art-e13nst.amvera.io/miniapp/tonconnect-manifest.json';
@@ -111,7 +111,7 @@ const App: React.FC = () => {
             <Route path="/profile" element={<MyProfilePage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
             <Route path="/author/:id" element={<AuthorPage />} />
-            <Route path="/generate-sticker" element={<StickerGeneratePage />} />
+            <Route path="/nft-soon" element={<NftSoonPage />} />
             {/* Fallback route */}
             <Route path="*" element={<DashboardPage />} />
           </Routes>
